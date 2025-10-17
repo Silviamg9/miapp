85a6a1e67bd1a9490082be2836de6b33cf39c93c
silviamg9 <moreno.garcia.silvia.maria@iescamas.es>
2025-10-17 20:57:22 +0200

    cambios botones

--- swing-base-app/src/main/java/com/example/swingapp/vista/ControlPanel.java
+++ swing-base-app/src/main/java/com/example/swingapp/vista/ControlPanel.java
@@ -15,11 +15,21 @@ public class ControlPanel extends JPanel {
         setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
 
         txtUrl = new JTextField ("https://example.com");
+        txtUrl.setForeground(new Color(255, 255, 255));
+        txtUrl.setBackground(new Color(0, 0, 0));
         btnDate  = new JButton("📅 Fecha");
+        btnDate.setForeground(new Color(139, 0, 139));
+        btnDate.setBackground(new Color(98, 160, 234));
         btnCurl = new JButton("🌐 Consultar");
+        btnCurl.setForeground(new Color(255, 255, 255));
+        btnCurl.setBackground(SystemColor.desktop);
         btnRun   = new JButton("▶ Ejecutar");
+        btnRun.setForeground(Color.RED);
+        btnRun.setBackground(new Color(111, 229, 158));
         btnClear = new JButton("🧹 Limpiar");
+        btnClear.setBackground(SystemColor.info);
         btnExit  = new JButton("🚪 Salir");
+        btnExit.setForeground(new Color(99, 69, 44));
 
         add(txtUrl);
         add(btnDate);
